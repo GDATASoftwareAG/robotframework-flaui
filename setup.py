f0c6e3fff44a0a87e2d150589ef251e12414be60
Andreas Sekulski <andreas.sekulski@gmail.com>
2023-04-04 21:06:21 +0200

    Adjust setup.py to use build version from package

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 import os
 from setuptools import setup, find_packages, Distribution
 
-# https://www.appveyor.com/docs/environment-variables/
-repo_tag = os.environ.get('APPVEYOR_REPO_TAG', 'false')
-tag_version = os.environ.get('APPVEYOR_REPO_TAG_NAME', '0')
-build_version = os.environ.get('APPVEYOR_BUILD_VERSION', '0')
-build_number = os.environ.get('APPVEYOR_BUILD_NUMBER', '0')
-release = repo_tag == 'true'
-
-print('---------------------Branch Informations---------------------')
-print('Release: ' + str(release))
-print('APPVEYOR_REPO_TAG: ' + repo_tag)
-print('APPVEYOR_REPO_TAG_NAME: ' + tag_version)
-print('APPVEYOR_BUILD_VERSION: ' + build_version)
-print('APPVEYOR_BUILD_NUMBER: ' + build_number)
-print('-------------------------------------------------------------')
-
 
 class BinaryDistribution(Distribution):
     """Distribution which always forces a binary package with platform name"""
@@ -26,18 +11,17 @@ class BinaryDistribution(Distribution):
         return True
 
 
-LONG_DESCRIPTION = ''
+with open("./src/FlaUILibrary/version.py", "r") as fh:
+    VersionFile = fh.read()
+    VersionFile = VersionFile.replace("\n", "")
+    IGNORE, VERSION = VersionFile.split(" = ")
+    VERSION = VERSION.replace("\"", "")
+
 with open("Readme.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
-REQUIREMENTS = []
 with open("requirements.txt", "r") as f:
     REQUIREMENTS = list(filter(lambda s: s != "", f.read().split("\n")))
-
-if release:
-    VERSION = "{}".format(tag_version)
-else:
-    VERSION = "{}rc{}".format(build_version, build_number)
 
 setup(name="robotframework-flaui",
       version=VERSION,
