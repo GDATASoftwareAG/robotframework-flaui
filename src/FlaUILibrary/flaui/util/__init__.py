4c34dd83901e3f5fb85fb3f69cc58ab9718243cb
Andreas Sekulski <andreas.sekulski@gmail.com>
2023-09-07 22:02:55 +0200

    #118 Find All Elements keyword

--- src/FlaUILibrary/flaui/util/__init__.py
+++ src/FlaUILibrary/flaui/util/__init__.py
@@ -2,3 +2,4 @@ from .keyboardinputconverter import KeyboardInputConverter
 from .treeitems import TreeItems
 from .treeitemsparser import TreeItemsParser
 from .automationinterfacecontainer import AutomationInterfaceContainer
+from .automationelement import AutomationElement
