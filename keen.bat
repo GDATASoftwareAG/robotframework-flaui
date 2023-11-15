26e0b3eca6caa422d525c00f4682b2ebfe3fbda5
Andreas Sekulski <andreas.sekulski@gmail.com>
2023-11-15 13:38:34 +0100

    Setup.py build process changes (#136)
    https://packaging.python.org/en/latest/discussions/setup-py-deprecated/
    
    Co-authored-by: Andreas Sekulski <Andreas.Sekulski@gdata.de>

--- keen.bat
+++ keen.bat
@@ -17,13 +17,13 @@ EXIT /B %ERRORLEVEL%
 :build
     call:cleanup
     call:dependency
-    call python setup.py sdist bdist_wheel
+    call python -m build
     call python libdoc.py
 EXIT /B %ERRORLEVEL%
 
 :install
     call:build
-    call pip install .
+    call python -m pip install .
 EXIT /B %ERRORLEVEL%
 
 :test_uia2
