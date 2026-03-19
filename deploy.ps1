189129ef4ce993518bf0816f2e57fc29223ab7e0
Andreas Sekulski <andreas.sekulski@gmail.com>
2026-03-19 15:02:56 +0100

    Fix for missing deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,15 @@
-if($env:APPVEYOR_REPO_TAG -eq 'true') {
+if ($env:APPVEYOR_REPO_TAG -eq 'true') {
     Write-Output ("Deploying " + $env:APPVEYOR_REPO_TAG_NAME + " to PyPI...")
+
+    python -m pip install -r requirements-dev.txt
+    if ($LASTEXITCODE -ne 0) { exit -1 }
+
     python -m twine upload --skip-existing dist/*.whl
+    if ($LASTEXITCODE -ne 0) { exit -1 }
+
     python -m twine upload --skip-existing dist/*.tar.gz
+    if ($LASTEXITCODE -ne 0) { exit -1 }
 }
-else
-{
+else {
     Write-Output "No tag for deployment"
 }
